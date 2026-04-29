2651d7e7d70b864ca563ca80ac53a1245575f942
DavidHC9 <davidherreracarvajal1@gmail.com>
2026-04-28 21:26:40 -0500

    just dance

--- src/phaser/scenes/BootScene.js
+++ src/phaser/scenes/BootScene.js
@@ -29,6 +29,7 @@ export class BootScene extends Phaser.Scene {
     this.load.image('keeper_neutral', 'assets/images/futbol/posicion1.png');
     this.load.image('keeper_side', 'assets/images/futbol/posicion iz.png');
     this.load.image('ball', 'assets/images/futbol/Balon.png');
+    this.load.image('neon_stage', 'assets/images/neon_stage.png');
   }
 
   create() {
